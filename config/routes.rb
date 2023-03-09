df208fa76ae55bf4a2eeb57c5dd1ec6b416515f2
J0nathanbell <jonnybell123@hotmail.com>
2023-03-09 18:25:03 +0000

    Users new and show added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:new, :create, :show, :update, :destroy]
 end
